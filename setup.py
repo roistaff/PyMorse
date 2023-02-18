0c3ef2e4d2dd35cc406866d533cfb109e5545374
roistaff <118814428+roistaff@users.noreply.github.com>
2023-02-19 01:22:00 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name='pymorce',
     version='0.1',
     packages=find_packages(),
-　description='This is a easy morse converter.Plese use FREE.if you find something wrong,plese tell me.',
+　description='This is a easy morse converter.Plese use FREE.if you found something wrong,plese tell me.',
    author='Roi Staff',
    author_email='roistaff1983@gmail.com'
 )
