f62c2455daa13c7420ddf494086ec678b2914ab5
roistaff <118814428+roistaff@users.noreply.github.com>
2023-02-19 11:09:20 +0900

    Update pymorse.py

--- pymorse/pymorse.py
+++ pymorse/pymorse.py
@@ -14,25 +14,22 @@ MORSEdic= { 'A':'.-', 'B':'-...',
                     '?':'..--..', '/':'-..-.', '-':'-....-',
                     '(':'-.--.', ')':'-.--.-',' ':','}
 class MORSE:
-  def string_to_code(self,input):
-    txt = str(input).upper()
-    code = ""
-    for s in txt:
-      code += MORSEdic[s]+"_"
-    code = code[:-1]
-    return code
-  def code_to_string(self,input):
-    MORSEdic2 = {v: k for k, v in MORSEdic.items()}
-    code = input.split("_")
-    txt = ""
-    try:
-      for s in code:
-        txt += MORSEdic2[s]
-    except:
-      txt += "?"
-    return txt
+    @classmethod
+    def string_to_code(self,input):
+        txt = str(input).upper()
+        code = ""
+        for s in txt:
+            code += MORSEdic[s]+"_"
+        code = code[:-1]
+        return code
+    @classmethod
+    def code_to_string(self,input):
+        MORSEdic2 = {v: k for k, v in MORSEdic.items()}
+        code = str(input).split("_")
+        txt = ""
+        for s in code:
+            txt += MORSEdic2[s]
+        return txt
 if __name__ == '__main__':
-  a = MORSE()
-  b = MORSE()
-  print(a.string_to_code("Hello world"))
-  print(b.code_to_string("...._._.-.._.-.._---_,_.--_---_.-._.-.._-.."))
+    print(MORSE.string_to_code("Hello world"))
+    print(MORSE.code_to_string("...._._.-.._.-.._---_,_.--_---_.-._.-.._-.."))
