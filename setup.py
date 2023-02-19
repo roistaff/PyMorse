00eb57ae4448fb124799077998fcb8bfed859409
roistaff <118814428+roistaff@users.noreply.github.com>
2023-02-19 11:59:45 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 from setuptools import setup, find_packages
-setup(name='pymorce',version='0.5',packages=find_packages(),description='This is a easy morse converter.Plese use FREE.if you found something wrong,plese tell me.',author='Roi Staff',author_email='roistaff1983@gmail.com')
+setup(name='pymorse',version='0.5',packages=find_packages(),description='This is a easy morse converter.Plese use FREE.if you found something wrong,plese tell me.',author='Roi Staff',author_email='roistaff1983@gmail.com')
